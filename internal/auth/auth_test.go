34f5daa4dec61b84caec7b549cb57daab303e4b7
AbdKaan <abdu.kaan@gmail.com>
2024-11-15 20:21:27 +0300

    unit test added with on purpose fail

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetBearerToken(t *testing.T) {
+	token := "ApiKey mytoken"
+	header := http.Header{"Authorization": {token}}
+	headerToken, err := GetAPIKey(header)
+	if err != nil {
+		t.Errorf("token format is wrong: %v", err)
+	}
+
+	if headerToken != "mytoken" {
+		t.Errorf("found tokens are not equal, %s != %s", headerToken, token)
+	}
+
+	t.Errorf("fail on purpose for testing purposes")
+}
